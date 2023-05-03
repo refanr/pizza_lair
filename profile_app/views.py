02c54ce248969dabc08ae209613a16231d2865a9
Reynir Gunnarsson <reynirg20@ru.is>
2023-05-03 12:09:14 +0000

    set up templates

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Hello from the index view in the profile app</h1>')
+    return render(request, 'profile_app/index.html')
