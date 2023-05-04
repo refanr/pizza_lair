e30eabf783e91098bf6205303f0f64d2e18ccbfa
Piparostur <birgirrs21@ru.is>
2023-05-04 16:11:25 +0000

    aðeins að testa views.py

--- pizza/views.py
+++ pizza/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from pizza.models import Pizza
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'pizza/index.html')
+
+# All pizzas
+def pizza_list(request):
+    pizzas = Pizza.objects.all()
+    return render(request, 'pizza/pizza_list.html', {'pizzas': pizzas})
+
+
+# Individual pizza object
+def pizza_detail(request, pk):
+    pizza = Pizza.objects.get(pk=pk)
+    return render(request, 'pizza/pizza_detail.html', {'pizza': pizza})
