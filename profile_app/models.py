12df3c3925410b0692fa99106cd95277f127dd65
Piparostur <birgirrs21@ru.is>
2023-05-04 15:35:42 +0000

    models fyrir user profile db

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Ef ég skil þetta rétt þá er þetta user profiles ?
+class user_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True, null=True)
