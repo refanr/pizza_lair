13d2a22b257d3592eed73707428229d8efbaa5dd
Piparostur <birgirrs21@ru.is>
2023-05-04 15:18:59 +0000

    pizza model fyrir database

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Models are used to create tables in the database
+# Each class is a table
+class Topping(models.Model):
+    name = models.CharField(max_length=50)
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    toppings = models.ManyToManyField(Topping)
