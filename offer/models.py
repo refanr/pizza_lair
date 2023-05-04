4196d28081c7d19efd2c3cf482988b398185825f
Piparostur <birgirrs21@ru.is>
2023-05-04 15:24:14 +0000

    Offer model fyrir db

--- offer/models.py
+++ offer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Offer(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+    # Spurning með þessa að neðan viljum líklega hafa mynd fyrir hvert tilboð og kannski start/end date eða ??
+    image = models.ImageField(upload_to='images/')
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
