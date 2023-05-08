a5aa137144f4b79e36a26cce948bed888deaa647
Piparostur <birgirrs21@ru.is>
2023-05-08 17:23:59 +0000

    sma utskyringar

--- offer/views.py
+++ offer/views.py
@@ -10,7 +10,7 @@ from pizza.models import Pizza
 #Pizza vikunar eeeeeeer MARGHARITA (erum að spara álegg)
 def index(request):
   pizza = Pizza.objects.get(name='Margherita')
-  discounted_price = pizza.price * 0.5
+  discounted_price = pizza.price * 0.5 # svaaaaaaakalega góð tilboð í gangi.
   return render(request, 'offer/index.html', {'pizza': pizza, 'discounted_price': discounted_price})
 # All offers view
 # def all_offers(request):
