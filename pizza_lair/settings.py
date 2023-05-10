ef35332f2bc4fa6cdbf5381c79d8cb06e2618287
Piparostur <birgirrs21@ru.is>
2023-05-10 15:31:06 +0000

    Cart clearast núna þegar þú lokar browser. Þurfum ekki lengur að bíða í tvær vikur.

--- pizza_lair/settings.py
+++ pizza_lair/settings.py
@@ -135,3 +135,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_AGE = 0
